1c760fd2f87f30f4372dbb3088f3a2504094b5dc
Ashwanth-kulal <kulalashwanth@gmail.com>
2026-02-12 13:56:32 +0530

    new commit form ash

---
+++ python practice/2b.py
@@ -0,0 +1,20 @@
+
+#concatenation
+
+first_name = "ashwanth"
+last_name = "kulal"
+full_name = first_name + " " + last_name 
+
+print(full_name)
+
+#repetition
+
+message = "warningertyuiop! " 
+print(message *10)
+
+
+name = "ashwanth"
+
+print(name[2])
+
+print(name[0:7])
---
+++ python practice/2c.py
@@ -0,0 +1,10 @@
+
+    #slicibng the string
+name= "ashwanth"
+
+
+print(name[3])  #To slice the only one letter : output is >> "w"
+print(name[1:6]) #To slice the word. how much we want: output is >> "shwan"
+print(name[ :6]) #To print the word from the begining to middle or half: Output is >> "ashwan"
+print(name[3: ]) #To print the word  from the middle of the word to the end: Out put is >> "wanth"
+print(name[0: ]) #To print the whole word: Output is >> "ashwanth" 
