3557992946007db5f949b7971e50771fa9d2594a
Ashwanth-kulal <kulalashwanth@gmail.com>
2026-02-13 13:13:10 +0530

    new commit about string manupulation

---
+++ python practice/String manupulation excercises.py
@@ -0,0 +1,8 @@
+message = input("enter your message: ")
+
+        #input is: "python is awesome"
+
+print(message.upper()) #To print the sentence in uppercase
+print(message.lower()) #To print the sentence in lowercase
+print(message.replace(" ", "_")) #To replace the speces into unerscore
+print(message.strip()) #To strip the sentences. it removes all the begining spaces and enging spaces ( it might be extra space or not)
