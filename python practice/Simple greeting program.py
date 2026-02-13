6fdff4eb48b361b8fe2d4b3458cd1ac7c800daa3
Ashwanth-kulal <kulalashwanth@gmail.com>
2026-02-13 12:29:27 +0530

    new commit by ash

--- python practice/Simple greeting program.py
+++ python practice/Simple greeting program.py
@@ -2,3 +2,5 @@ name = " Ashwanth"
 age = "23"
 
 print( "hello," + name + "!" + " your age is "  + age + " years old ")  # hello, Ashwanth! your age is 23 years old 
+
+print(f" hello, {name}! your age is {age} years old")  # hello, Ashwanth! your age is 23 years old 
